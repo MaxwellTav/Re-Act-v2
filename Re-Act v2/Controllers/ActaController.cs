ee9c7521f49f1d8625b3835c1b1e7efb08917ff5
Max <Max@DESKTOP-GLNEN2J>
2024-04-11 01:23:02 -0400

    Implementación básica de la vista del formulario presidencial.

---
+++ Re-Act v2/Controllers/ActaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Re_Act_v2.Controllers
+{
+    public class ActaController : Controller
+    {
+        public static int dummyCuantityCandidates = 34;
+        public IActionResult AddVotesPresident()
+        {
+            return View(dummyCuantityCandidates);
+        }
+    }
+}
