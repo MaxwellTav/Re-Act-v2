d3fb832d08947bc487afdcb8cf8af6d0724b5c91
Max <Max@DESKTOP-GLNEN2J>
2024-04-16 01:08:20 -0400

    Modificación a la DB.
    Creación de las tablas:
    Recint_Table
    ElectoralTable_Table
    
    E insercción de datos de las mismas.
    
    Modificación del _Sidebar.
    Se agregó la pestaña de "Consulta".

--- Re-Act v2/Documentation/ReActDBQuery.sql
+++ Re-Act v2/Documentation/ReActDBQuery.sql
@@ -32,10 +32,86 @@ Candidate_Role Int Not Null,
 Candidate_VotesCuantity Int Not Null Default(0),
 FOREIGN KEY (Candidate_Role) REFERENCES Role_Table(CandidateRole_ID));
 
+Create Table Recint_Table
+(Recint_ID Int Primary Key Identity(0, 1),
+Recint_Name Varchar(100) Unique Not Null);
+
+Create Table ElectoralTable_Table
+(ElectoralTable_ID Int Primary Key Identity(0, 1),
+ElectoralTable_Name Varchar(6) Not Null Unique,
+Recint_ID Int Not Null,
+ElectoralTable_Votes Int Default(0),
+Foreign Key (Recint_ID) References Recint_Table(Recint_ID));
+
+
 -- ####################  Insertar datos  ####################
+--Rol.
 Insert Into Role_Table Values ('Presidente');
 Insert Into Role_Table Values ('Senador');
 Insert Into Role_Table Values ('Diputado');
+
+--Recintos.
+Insert Into Recint_Table Values 
+('ESCUELA PRIMARIA REPUBLICA DE ARUBA'), 
+('ESCUELA PARROQUIAL CRISTO REY'),
+('LICEO BENITO JUAREZ'),
+('ESCUELA BASICA PROF. JUAN BOSCH'),
+('ESCUELA PUBLICA SANTA MARTHA'),
+('CLUB DEPORTIVO Y CULTURAL LOS CACHORROS'),
+('COLEGIO SANTA RITA'),
+('INSTITUTO POLITECNICO ANGELES CUSTODIO'),
+('LICEO PARROQUIAL SAN PABLO APOSTOL'),
+('COLEGIO ARROYO HONDO'),
+('ESCUELA REPUBLICA DE COSTA RICA'),
+('LICEO TECNICO MARILLAC'),
+('CUERPO DE BOMBEROS CIVILES'),
+('ESCUELA PRIMARIA FIDEL FERRER'),
+('CLUB DEL MINISTERIO DE OBRAS PUBLICAS Y COMUNICACION'),
+('MINISTERIO DE SALUD PUBLICA Y ASISTENCIA SOCIAL'),
+('CENTRO EDUCATIVO LA MILAGROSA'),
+('ESCUELA PUBLICA PROF. FRANCISCO ULISES DOMINGUEZ'),
+('ESCUELA PRIMARIA JOSE BORDAS VALDEZ'),
+('ESCUELA PRIMARIA JESUS MAESTRO'),
+('SOC. SAGRADO CORAZON DE JESUS'),
+('COLEGIO NIDO PARA LOS ANGELES'),
+('UNIVERSIDAD NACIONAL PEDRO HENRIQUEZ URENA (UNPHU)'),
+('MODULO C-4 DE LA OMSA'),
+('CENTRO JUVENIL CRISTO REY'),
+('ESCUELA BASICA FRANCISCO DEL ROSARIO SANCHEZ'),
+('INSTITUTO POLITECNICO VICTOR ESTRELLA LIZ (PERITO)'),
+('ESCUELA BASICA ARROYO HONDO II'),
+('CENTRO EDUCATIVO PROF. ISIDRO PEREZ BELLO'),
+('COLEGIO PALMA REAL'),
+('CENTRO EDUCATIVO SAN JOSE FE Y ALEGRIA (LADO ESTE)'),
+('CENTRO PEDAGOGICO ESTRELLA DE VIDA'),
+('CLUB DEPORTIVO Y CULTURAL JARDINES DEL NORTE'),
+('COLEGIO FALLOW ME'),
+('CENTRO EDUCATIVO SAN MAURICIO'),
+('COLEGIO CRISTIANO REFORMADO EMANUEL'),
+('ESCUELA MALAQUIAS GIL'),
+('CLUB 21 DE ENERO'),
+('ESCUELA BASICA PADRE EULALIO A. ARIAS'),
+('CENTRO DE ESTUDIO BASICA MI SEGUNDO HOGAR'),
+('ESCUELA BASICA JULIAN AMPARO'),
+('COLEGIO CICRE'),
+('COLEGIO NUESTRA SEÑORA DE LOURDES'),
+('ESCUELA DOÑA SOCORRO'),
+('ESCUELA MARlA DEL CARMEN PREZ'),
+('ESCUELA PRIMARIA BARTOLOM DE LAS CASAS'),
+('ESCUELA PRIMARIA PUERTO ISABELA (NUEVA)'),
+('ESTACION DE BOMBEROS CORONEL FRANCISCO DESANGLES'),
+('INSTITUTO TECNOLOGICO DE SANTO DOMINGO (INTEC)'),
+('CENTRO EDUCATIVO SAN BENITO');
+
+--Mesas y colegios.
+Insert Into ElectoralTable_Table Values
+--00028     ESCUELA PRIMARIA REPUBLICA DE ARUBA							
+('1448', 0, 0),
+('1448A', 0, 0), 
+('1450', 0, 0), 
+('1450A', 0, 0), 
+('1537', 0, 0), 
+('1538', 0, 0);
 
 -- _________________ Insertar datos dummy ___________________
 --Partidos
@@ -65,9 +141,16 @@ Insert Into PoliticalParty_Table Values ('AllyTree',
 Insert Into PoliticalAlliance_Table Values (0, 3);
 Insert Into PoliticalAlliance_Table Values (1, 4);
 Insert Into PoliticalAlliance_Table Values (2, 5);
-
 -- #################### Todas las Vistas ####################
 
+Select * From PoliticalParty_Table;
+Select * From Candidates_Table;
+Select * From Role_Table;
+Select * From AspNetUsers;
+Select * From Recint_Table;
+Select * From ElectoralTable_Table;
+
+--Todos los partidos políticos
 SELECT
     pa.PoliticalAlliance_ID As '#ID',
     pb.PoliticalParty_Name AS 'Partido político Beneficiado',
@@ -80,11 +163,33 @@ JOIN
     PoliticalParty_Table pb ON pa.PoliticalAlliance_Beneficiary = pb.PoliticalParty_ID
 JOIN
     PoliticalParty_Table pr ON pa.PoliticalAlliance_Remitent = pr.PoliticalParty_ID;
-	--______
 
-Select * From PoliticalParty_Table;
-Select * From Candidates_Table;
-Select * From Role_Table;
-Select * From AspNetUsers;
+--Todas las mesas.
+SELECT Recint_Table.Recint_Name, ElectoralTable_Table.ElectoralTable_Name
+FROM ElectoralTable_Table
+INNER JOIN Recint_Table ON ElectoralTable_Table.Recint_ID = Recint_Table.Recint_ID;
+
+--Total votos.
+SELECT 
+    Recint_Name,
+    (
+        SELECT SUM(ElectoralTable_Votes) 
+        FROM ElectoralTable_Table 
+        WHERE ElectoralTable_Table.Recint_ID = Recint_Table.Recint_ID
+    ) AS Total_Votes
+FROM 
+    Recint_Table;
+
+--Sumatoria de todos los recintos.
+SELECT 
+    Recint_Table.Recint_Name,
+    COUNT(ElectoralTable_Table.ElectoralTable_ID) AS Total_Mesas
+FROM 
+    Recint_Table
+LEFT JOIN 
+    ElectoralTable_Table ON Recint_Table.Recint_ID = ElectoralTable_Table.Recint_ID
+GROUP BY 
+    Recint_Table.Recint_Name;
+
 
 -- #################### __________________ ####################
